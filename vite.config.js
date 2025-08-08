b4a197069b7785ddb020a0872fbf2554b52b2e19
Tharindu Ranaweera <nawanjanaenda@gmail.com>
2025-08-08 13:13:00 +0530

    change index file

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import viteCompression from 'vite-plugin-compression'
+import ssr from 'vite-plugin-ssr/plugin'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteCompression(),
+    ssr() // ✅ Add this line
+  ]
 })
