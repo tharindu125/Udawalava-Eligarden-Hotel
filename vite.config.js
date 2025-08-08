ae1a03c25e5adad8e046c2ad5245d4248341b111
Tharindu Ranaweera <nawanjanaenda@gmail.com>
2025-08-08 23:58:36 +0530

    issue fix

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './', // ⬅️ This is important for relative paths
   plugins: [react()],
 })
